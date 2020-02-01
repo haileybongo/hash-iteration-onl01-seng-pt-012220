42846be4ef5271596a7726e649fb9d8c41739880
Hailey Bongo <haileyebongo@gmail.com>
2020-02-01 01:46:55 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,9 +11,10 @@ def happy_birthday(birthday_kids)
   end
 end
 
-def happy_birthday(birthday_kids)
+def age_appropriate_birthday(birthday_kids)
   # add your code snippet here!
   birthday_kids.each do |kids_name, age|
+    if age <= 
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
